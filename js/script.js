80d4b5b0d0e9e4b206ebb74ca467d7541127382a
lorenzosalti <lorenzosalti@gmail.com>
2025-03-06 16:32:24 +0100

    feat: countdown stop function

--- js/script.js
+++ js/script.js
@@ -5,22 +5,23 @@ const maxNumber = 50;
 
 // variabili elementi html
 const numbersListElement = document.getElementById('numbers-list');
+const answersForm = document.getElementById('answers-form');
 
 
-
-
+// creazione e visualizzazione dei numeri randomici
 for (i = 0; i < 5; i++) {
   const currentNumber = getRandom(minNumber, maxNumber);
   randoms.push(currentNumber);
   console.log(currentNumber);
 
-  numbersListElement.innerHTML += `<li>${currentNumber}</li>`;
+  numbersListElement.innerHTML += `<li class="random-number">${currentNumber}</li>`;
 }
 
 console.log(randoms);
 
 
 
+// setTimeout(countdownStop, 10_000, 10);
 
 
 
@@ -28,4 +29,11 @@ console.log(randoms);
 //genera un numero intero random tra min e max inclusi
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function countdownStop(clear) {
+  numbersListElement.classList.add('d-none');
+  answersForm.classList.remove('d-none');
+  clearInterval(clear);
 }
